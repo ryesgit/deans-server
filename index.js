a7babde948b18192d2463dacc98499ee6f7bec61
Rey <100455152+codenamerey@users.noreply.github.com>
2025-11-25 12:26:25 +0800

    feat: increase image upload limit

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 await initializeDatabase();
 
